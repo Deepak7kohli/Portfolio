8e741361667de8d5cd8448f1f2c6c52a3b6cb436
Deepak Kumar <deepakdkay201617@gmail.com>
2026-03-07 14:24:24 +0530

    fix case sensitive import path

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { ThemeProvider } from "./assets/Common/themecontext.jsx";
+import { ThemeProvider } from "./assets/Common/themeContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
