cbde879ecd7f561739867f7d1df8042cf65ecb08
Deepak Kumar <deepakdkay201617@gmail.com>
2026-03-07 14:48:23 +0530

    fix case sensitive import path

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ import "./App.css"
 import Hero from "./sections/Hero/Hero.jsx"
 import Projects from "./sections/Projects/Projects.jsx"
 import Skills from "./sections/skill/skills.jsx"
-import Contact from "./sections/Contact/contact.jsx"
+// import Contact from "./sections/Contact/contact.jsx"
+import Contact from "./sections/Contact/Contact.jsx";
+
 import Footter from "./sections/Footer/footter.jsx"
 
 function App() {
